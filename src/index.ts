0f24fd85003cdab0c624960a7b6bdd14d248e317
Vlad Sydorenko <vladv.sydorenko@gmail.com>
2021-07-11 18:27:31 +0300

    remake index and structure

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-import './test';
+export { Entity } from './Entity';
+export { EntityContainer } from './EntityContainer';
+export { EntityContainerManager } from './EntityContainerManager';
+export { EntityManager } from './EntityManager';
+export { EntityQuery } from './EntityQuery';
+
+export { Sign } from './Sign';
+export { SignManager } from './SignManager';
+export { Type } from './Type';
+export { TypeManager } from './TypeManager';
